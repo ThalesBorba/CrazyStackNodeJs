f8df66ccb4340f4c9f14718f348dbb677c1fe057
Thales <Thales_borba@hotmail.com>
2024-05-20 19:57:37 -0300

    Aula 8

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -17,7 +17,58 @@ module.exports = function (plop) {
         ],
         actions: [...entitiesCreations],
     });
+    plop.setGenerator("useCases", {
+        description: "Create a new useCase",
+        prompts: [
+            {
+                type: "input",
+                name: "name",
+                message: "What is the name of the useCase?",
+            },
+        ],
+        actions: [...useCaseCreations],
+    });
+    plop.setGenerator("test", {
+        description: "Create a new test",
+        prompts: [
+            {
+                type: "input",
+                name: "name",
+                message: "What is the name of the file you'll test?",
+            },
+            {
+                type: "input",
+                name: "entity",
+                message: "What is the name of the entity?",
+            },
+            {
+                type: "input",
+                name: "layer",
+                message: "What is the name of the layer?",
+            }
+        ],
+        actions: [
+            {
+                type: "add",
+                path: "../src/slices/{{camelCase entity}}/{{camelCase layer}}/{{camelCase name}}Test.spec.ts",
+                templateFile: "./templates/test.spec.ts.hbs"
+            },
+],
+    });
 };
+
+const useCaseCreations = [
+    {
+        type: "add",
+        path: "../src/slices/{{camelCase name}}/repositories/contracts/index.ts",
+        templateFile: "./templates/repositories/contracts/index.ts.hbs"
+    },
+    {
+        type: "add",
+        path: "../src/slices/{{camelCase name}}/repositories/contracts/add{{pascalCase name}}Repository.ts",
+        templateFile: "./templates/repositories/contracts/addDomainRepository.ts.hbs"
+    }
+]
 
 const entitiesCreations = [
     {
